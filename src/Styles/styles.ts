1a74a65b72f465204ac809d9618f1bf3e5a38a5e
Ian Steiger <iksteiger@gmail.com>
2021-04-14 13:50:46 -0400

    things

---
+++ src/Styles/styles.ts
@@ -0,0 +1,61 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { ClassNameMap } from "@material-ui/styles";
+
+const drawerWidth = 280; // Main drawer width
+const useStyles: () => ClassNameMap<string> = // Define the styles we will use elsewhere
+    makeStyles((theme: Theme) => createStyles({
+        appBar: {
+            backgroundColor: "#003E81",
+            transition: theme.transitions.create(["margin", "width"], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            })
+        },
+        appBarShift: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            transition: theme.transitions.create(["margin", "width"], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        hide: {
+            display: "none",
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerHeader: {
+            display: "flex",
+            alignItems: "center",
+            padding: theme.spacing(0, 1),
+            // necessary for content to be below app bar
+            ...theme.mixins.toolbar,
+            justifyContent: "flex-end",
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+            transition: theme.transitions.create("margin", {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -drawerWidth,
+        },
+        contentShift: {
+            transition: theme.transitions.create("margin", {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        },
+    }));
+
+export default useStyles; // Export the styles for use elsewhere
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "normalize.css"; // Import normalize.css (a css reset)
 
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar } from "@material-ui/core"; // Import our Material-UI Components
+import { ClassNameMap } from "@material-ui/core/styles/withStyles";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"; // Import Chevron Icon
+import MenuIcon from "@material-ui/icons/Menu"; // Import Menu Icon
+import clsx from "clsx"; // Import conditional React class helper
+import React from "react";
+import ReactDOM from "react-dom"; // Import React DOM
+
+import useStyles from "./Styles/styles"; // Import our styles
+
+function MainApp() {
+    const globalStyles: ClassNameMap<string> = useStyles(); // Get styles
+    const [drawerOpen, drawerSetOpen] = React.useState<boolean>(false); // Drawer open state
+    const handleDrawerOpen = () => { // Function for drawer open
+        drawerSetOpen(true);
+    };
+
+    const handleDrawerClose = () => { // Function for drawer close
+        drawerSetOpen(false);
+    };
+    return (
+        <>
+            <AppBar position="static" className={clsx(globalStyles.appBar, {
+                [globalStyles.appBarShift]: drawerOpen,
+            })}>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon onClick={handleDrawerOpen} className={clsx(globalStyles.menuButton, drawerOpen && globalStyles.hide)} />
+                    </IconButton>
+                    <img src="/assets/images/transparentLogo.svg" alt="PinkertonMC logo" height="36" />
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={globalStyles.drawer}
+                variant="persistent"
+                anchor="left"
+                open={drawerOpen}
+                classes={{
+                    paper: globalStyles.drawerPaper,
+                }}
+            >
+                <div className={globalStyles.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+
+                <List>
+                    <ListItem button>
+                        <ListItemIcon><MenuIcon /></ListItemIcon>
+                        <ListItemText primary="Test" />
+                    </ListItem>
+                </List>
+            </Drawer>
+            <main className={clsx(globalStyles.content, {
+                [globalStyles.contentShift]: drawerOpen,
+            })} >
+            </main>
+        </>
+    )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <MainApp />,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

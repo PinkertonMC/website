1debf2fc3bd318064a903badb32518f05e3154bc
Ian Steiger <iksteiger@gmail.com>
2021-09-03 18:02:19 -0400

    fix copying text

--- src/Snippets/Copy.ts
+++ src/Snippets/Copy.ts
@@ -1,8 +1,10 @@
 export default function copy(that: string) {
-    let inp: any = document.createElement("input");
-    document.body.appendChild(inp);
-    inp.value = that;
-    inp.select();
-    document.execCommand("copy", false);
-    inp.remove();
+    /*let inp: any = document.createElement("input");
+document.body.appendChild(inp);
+inp.value = that;
+inp.select();
+document.execCommand("copy", false);
+inp.remove();
+navigator.clipboard.writeText(that)*/
+    navigator.clipboard.writeText(that);
 }
